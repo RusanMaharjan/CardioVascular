57f8248c1177235ff91a25ac25e45c6d3125bf34
Rusan <61351604+RusanMaharjan@users.noreply.github.com>
2026-07-08 10:08:55 +0545

    Backend

---
+++ app/schema.py
@@ -0,0 +1,16 @@
+# Data Structure
+
+from pydantic import BaseModel
+
+class Cardio(BaseModel):
+    age: int
+    gender: int
+    height: float
+    weight: int
+    ap_hi: int
+    ap_lo: int
+    cholesterol: int
+    gluc: int
+    smoke: int
+    alco: int
+    active: int
